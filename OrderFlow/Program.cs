c302205d48a246508190d0195f08969e6c77f613
Filip <f.bartosiak.p@gmail.com>
2026-04-02 19:46:35 +0200

    Zadanie 3

--- OrderFlow/Program.cs
+++ OrderFlow/Program.cs
@@ -1,31 +1,40 @@
-﻿using System.Diagnostics;
-using OrderFlow.Data;
+﻿using OrderFlow.Data;
 using OrderFlow.Services;
 
-Console.WriteLine("=== LAB 2 | ZADANIE 2: ASYNCHRONICZNOŚĆ ===\n");
+Console.WriteLine("=== LAB 2 | ZADANIE 3: THREAD SAFETY ===\n");
 
-var orders = SampleData.Orders.Where(o => o.Pozycje.Count > 0).ToList();
-var externalServices = new ExternalServiceSimulator();
-var sw = new Stopwatch();
-
-Console.WriteLine("--- 1. Przetwarzanie SEKWENCYJNE (po kolei) ---");
-sw.Start();
-foreach (var order in orders)
+var massiveOrderList = new List<Order>();
+for (int i = 0; i < 100_000; i++)
 {
-    await externalServices.ProcessOrderAsync(order);
+    massiveOrderList.AddRange(SampleData.Orders);
 }
-sw.Stop();
-var sekwencyjnieCzas = sw.ElapsedMilliseconds;
 
+int expectedCount = massiveOrderList.Count;
+decimal expectedRevenue = 0;
+foreach (var o in massiveOrderList) expectedRevenue += o.TotalAmount;
 
-Console.WriteLine("\n--- 2. Przetwarzanie RÓWNOLEGŁE (max 3 na raz) ---");
-sw.Restart();
-await externalServices.ProcessMultipleOrdersAsync(orders);
-sw.Stop();
-var rownolegleCzas = sw.ElapsedMilliseconds;
+Console.WriteLine($"Oczekiwana liczba zamówień: {expectedCount}");
+Console.WriteLine($"Oczekiwany łączny przychód:  {expectedRevenue:C}\n");
 
-Console.WriteLine("\n=== PODSUMOWANIE CZASOWE ===");
-Console.WriteLine($"Czas sekwencyjnie: {sekwencyjnieCzas} ms");
-Console.WriteLine($"Czas równolegle:   {rownolegleCzas} ms");
+var stats = new OrderStatistics();
+
+Console.WriteLine("--- 1. Przetwarzanie RÓWNOLEGŁE (Bez synchronizacji) ---");
+stats.CalculateUnsafe(massiveOrderList);
+
+Console.WriteLine($"Otrzymana liczba zamówień:   {stats.TotalProcessed} ❌ (Zgubiono: {expectedCount - stats.TotalProcessed})");
+Console.WriteLine($"Otrzymany łączny przychód:   {stats.TotalRevenue:C} ❌");
+
+stats.Reset();
+
+Console.WriteLine("\n--- 2. Przetwarzanie RÓWNOLEGŁE (Bezpieczne - lock & Interlocked) ---");
+stats.CalculateSafe(massiveOrderList);
+
+Console.WriteLine($"Otrzymana liczba zamówień:   {stats.TotalProcessed} ✅ (Zgubiono: 0)");
+Console.WriteLine($"Otrzymany łączny przychód:   {stats.TotalRevenue:C} ✅");
+Console.WriteLine("\nPodział na statusy (ConcurrentDictionary):");
+foreach (var status in stats.SafeOrdersPerStatus)
+{
+    Console.WriteLine($" - {status.Key}: {status.Value}");
+}
 
 Console.ReadLine();
